3a4b027969cc472241bac1e869f44765ba2bb40e
bullerhenrik <henrik.buller@omegapoint.se>
2024-08-26 20:46:47 +0200

    Refactor API response

--- src/main/java/se/omegapoint/HelloWorldHandler.java
+++ src/main/java/se/omegapoint/HelloWorldHandler.java
@@ -4,17 +4,23 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
+import se.omegapoint.helloworld.dto.HelloWorldResponse;
+
 
 @Slf4j
 public class HelloWorldHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     private final HelloService helloService;
     private final WorldService worldService;
+    private final ObjectMapper mapper;
 
     public HelloWorldHandler() {
         this.helloService = new HelloService();
         this.worldService = new WorldService();
+        this.mapper = new ObjectMapper();
 
         var hello = helloService.getHello();
         var world = worldService.getWorld();
@@ -28,7 +34,13 @@ public class HelloWorldHandler implements RequestHandler<APIGatewayProxyRequestE
         var world = worldService.getWorld();
         var message = "%s %s!".formatted(hello, world);
 
-        log.debug("Request handled: {}", message);
-        return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(message);
+        var response = new HelloWorldResponse();
+        response.setMessage(message);
+
+        try {
+            return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody(mapper.writeValueAsString(response));
+        } catch (JsonProcessingException e) {
+            return new APIGatewayProxyResponseEvent().withStatusCode(500);
+        }
     }
 }
