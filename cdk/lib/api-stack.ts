1642d330084d46c5b40b6c6884e6b085a5758113
bullerhenrik <henrik.buller@omegapoint.se>
2024-09-12 08:47:24 +0200

    Create OpenApiGatewayToLambda construct

--- cdk/lib/api-stack.ts
+++ cdk/lib/api-stack.ts
@@ -1,50 +1,38 @@
 import * as cdk from "aws-cdk-lib"
-import { aws_lambda, Fn, StackProps } from "aws-cdk-lib"
+import { aws_lambda, StackProps } from "aws-cdk-lib"
 import { Construct } from "constructs"
-import { ApiDefinition, SpecRestApi } from "aws-cdk-lib/aws-apigateway"
 import { Asset } from "aws-cdk-lib/aws-s3-assets"
 import * as path from "path"
-import { CfnRole, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam"
-import { CfnFunction, SnapStartConf } from "aws-cdk-lib/aws-lambda"
+import { SnapStartConf } from "aws-cdk-lib/aws-lambda"
+import { OpenApiGatewayToLambda } from "./constructs/open-api-gateway-to-lambda"
 
 interface ApiStackProps extends StackProps {
   version: string
 }
 
 export class ApiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: ApiStackProps) {
+  constructor(scope: Construct, id: string, props: ApiStackProps) {
     super(scope, id, props)
 
-    // Lambda
-    const lambda = new aws_lambda.Function(this, "HelloWorldLambda", {
-      runtime: aws_lambda.Runtime.JAVA_21,
-      handler: "se.omegapoint.HelloWorldHandler",
-      code: aws_lambda.Code.fromAsset(path.join(__dirname, `../../target/op-invent-java-lambda-${props?.version}-SNAPSHOT.jar`)),
-      memorySize: 2048,
-      snapStart: SnapStartConf.ON_PUBLISHED_VERSIONS,
-    })
-
-    ;(lambda.node.defaultChild as CfnFunction).overrideLogicalId("HelloWorldLambda")
-
-    lambda.addAlias("current")
-
-    // Role
-    const apiRole = new Role(this, "ApiLambdaRole", {
-      assumedBy: new ServicePrincipal("apigateway.amazonaws.com"),
-    })
-
-    ;(apiRole.node.defaultChild as CfnRole).overrideLogicalId("ApiLambdaRole")
-
-    lambda.grantInvoke(apiRole)
-
-    // Api Gateway
-    const apiAsset = new Asset(this, "ApiSpecAsset", {
-      path: path.join("../api/hello-world-v1.yaml"),
-    })
-    const apiData = Fn.transform("AWS::Include", { location: apiAsset.s3ObjectUrl })
-
-    new SpecRestApi(this, "HelloWorldApi", {
-      apiDefinition: ApiDefinition.fromInline(apiData),
+    new OpenApiGatewayToLambda(this, "HelloWorldApi", {
+      api: {
+        id: "HelloWorldApiGateway",
+        roleId: "ApiLambdaRole",
+        apiDefinitionAsset: new Asset(this, "ApiSpecAsset", {
+          path: path.join("../api/hello-world-v1.yaml"),
+        }),
+      },
+      apiIntegration: [
+        {
+          id: "HelloWorldLambda",
+          runtime: aws_lambda.Runtime.JAVA_21,
+          handler: "se.omegapoint.HelloWorldHandler",
+          code: aws_lambda.Code.fromAsset(path.join(__dirname, `../../target/op-invent-java-lambda-${props.version}-SNAPSHOT.jar`)),
+          memorySize: 2048,
+          snapStart: SnapStartConf.ON_PUBLISHED_VERSIONS,
+          version: props.version,
+        },
+      ],
     })
   }
 }
---
+++ cdk/lib/constructs/open-api-gateway-to-lambda.ts
@@ -0,0 +1,69 @@
+import {Construct} from "constructs";
+import {
+    CfnFunction,
+    Code,
+    Runtime,
+    SnapStartConf
+} from "aws-cdk-lib/aws-lambda";
+import {CfnRole, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
+import {Asset} from "aws-cdk-lib/aws-s3-assets";
+import {ApiDefinition, SpecRestApi} from "aws-cdk-lib/aws-apigateway";
+import {aws_lambda, Fn} from "aws-cdk-lib";
+
+
+interface ApiIntegration {
+    /** Id of the ApiIntegration, used to correlate this lambda function to the api integration in the open api definition. */
+    id: string;
+    runtime: Runtime;
+    handler: string;
+    code: Code;
+    snapStart?: SnapStartConf;
+    memorySize?: number;
+    version: string;
+}
+
+interface OpenApiGatewayToLambdaProps {
+    api: {
+        id: string;
+        /** Id of the API role, used to correlate the role created to the api integration credentials in the open api definition. */
+        roleId: string
+        /** Local file asset of the OpenAPI spec file. */
+        apiDefinitionAsset: Asset
+    },
+    apiIntegration: ApiIntegration[]
+}
+
+export class OpenApiGatewayToLambda extends Construct {
+
+    constructor(scope: Construct, id: string, props: OpenApiGatewayToLambdaProps) {
+        super(scope, id);
+
+        const apiRole = new Role(this, props.api.roleId, {
+                assumedBy: new ServicePrincipal("apigateway.amazonaws.com"),
+            })
+
+        ;(apiRole.node.defaultChild as CfnRole).overrideLogicalId(props.api.roleId)
+
+        props.apiIntegration.map(apiIntegration => {
+            const lambda = new aws_lambda.Function(this, apiIntegration.id, {
+                    runtime: apiIntegration.runtime,
+                    handler: apiIntegration.handler,
+                    code: apiIntegration.code,
+                    memorySize: apiIntegration.memorySize ?? undefined,
+                    snapStart: apiIntegration.snapStart ?? undefined,
+                })
+
+             ;(lambda.node.defaultChild as CfnFunction).overrideLogicalId(apiIntegration.id)
+            lambda.addAlias("current")
+            lambda.grantInvoke(apiRole)
+
+            return lambda
+        })
+
+        const apiData = Fn.transform("AWS::Include", {location: props.api.apiDefinitionAsset.s3ObjectUrl})
+
+        new SpecRestApi(this, props.api.id, {
+            apiDefinition: ApiDefinition.fromInline(apiData),
+        })
+    }
+}
